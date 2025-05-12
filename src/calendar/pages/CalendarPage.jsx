91cdabfd2bce06c02f4dbdc0b0cbea3a995e8577
Miguel Guirola <miguel.guirola@outlook.com>
2025-05-12 15:45:02 -0600

    Prueba DC  y C

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,31 +1,21 @@
-import { Calendar, dateFnsLocalizer, Views } from 'react-big-calendar'
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-
-import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
-import enUS from 'date-fns/locale/en-US'
-
-import { Navbar } from "../"
 import { useState } from 'react';
 
+import { Calendar, Views } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-const locales = {
-  'en-US': enUS,
-}
+import { addHours } from 'date-fns'
+import useDoubleClick from 'use-double-click';
 
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-});
+import { CalendarEvent, Navbar } from "../"
+import { localizer, getMessagesES } from '../../helpers';
+
 
 const events = [{
   title: 'Cumpleaños del jefe',
   notes: 'Hay que comprar el pastel',
   start: new Date(),
   end: addHours( new(Date), 2 ),
-  bgColor: '#fafafa',
+  bgColor: '#fAfAfA',
   user: {
     _id: '123',
     name: 'Miguel'
@@ -37,11 +27,43 @@ export const CalendarPage = () => {
   const [currentView, setCurrentView] = useState(Views.MONTH);
   const [currentDate, setCurrentDate] = useState(new Date());
 
+  let timer;
+  const delay = 300; 
+
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+
+    const style = {
+      backgroundColor: '#4169E1',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return {
+      style
+    }
+  }
+
+  
+  const onDoubleClick = ( event ) => {
+    clearTimeout(timer); // Clear the timer to prevent single click action
+      // Perform double click action
+      console.log({Doubleclick: event});
+  }
+
+  const onSelect = ( event ) => {
+    timer = setTimeout(() => {
+        // Perform single click action only if not cleared by double click
+        console.log({Click: event }); 
+      }, delay);   
+  }
+
   return (
     <>
       <Navbar />
 
       <Calendar
+        culture='es'
         localizer={ localizer }
         events={ events }
         startAccessor="start"
@@ -50,7 +72,14 @@ export const CalendarPage = () => {
         view={ currentView }
         onView={ setCurrentView }
         onNavigate={ setCurrentDate }
-        style={{ height: 'cal( 100vh - 80px )' }}
+        style={{ height: 'calc( 100vh - 80px )' }}
+        messages={ getMessagesES() }
+        eventPropGetter={ eventStyleGetter }
+        components={{
+          event: CalendarEvent
+        }}
+        onDoubleClickEvent={ onDoubleClick }    
+        onSelectEvent={ onSelect }    
     />
 
     </>
